902349b8a740f51cb1f54ac75a9a0c56a09f8a81
Jessica Nelson <yahwehschica@yahoo.com>
2021-12-11 17:06:37 -0500

    first fetch api written

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-fetch("") 
+const baseUrl = "https://api.fbi.gov/@wanted"
+
+fetch(baseUrl)
+.then(res => res.json())
+.then(res => console.log(res))
+
+
+const card = document.createElement("div")
+card.style = "blue"
