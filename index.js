6c008ba9b92545b79bdea5c87d528718a758aaa3
Jessica Nelson <yahwehschica@yahoo.com>
2021-12-13 08:18:41 -0500

    made img function

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
+
+
+
 // need a nav bar, sidebars, footers
+
+
 
 // const h1 = document.querySelector("h1")
 // const h2 = document.createElement("h2")
@@ -15,7 +20,12 @@ function fetchApi() {
 .then(res => console.log(res))
 }
 
-
+function myCat() {
+    const img = new Image ()
+    img.src = "https://img-9gag-fun.9cache.com/photo/aD1n89B_460s.jpg"
+    document.getElementById("body").appendChild(img);
+    
+}
 // const contactForm = document.createElement("form"); 
 // contactForm.setAttribute("type", "text");
 // document.body.appendChild(contactForm);
