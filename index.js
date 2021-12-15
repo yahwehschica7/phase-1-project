700521b8f45f43dede619a6a3f00db511b5958ab
Jessica Nelson <yahwehschica@yahoo.com>
2021-12-15 08:32:33 -0500

    created container div for image, api, form

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
-const contactForm = document.createElement("form")
-contactForm.setAttribute
-
+const mainDiv = () => document.getElementById("main")
 
 const baseUrl = "https://api.fbi.gov/@wanted"
+const homePageTemplate = () => {
+    return `<h3>In Search of Truth</h3>`
+}
+
 function fetchApi() {
-    fetch(baseUrl)
+fetch(baseUrl)
 .then(res => res.json())
 .then(res => console.log(res))
 }
 
-function myCat() {
+const myCat = () => {
     const img = new Image ()
     img.src = "https://img-9gag-fun.9cache.com/photo/aD1n89B_460s.jpg"
     img.width ="250"
     img.height = "300"
-    document.getElementById("body").appendChild(img);
-    const input = document.createElement("input")
-    const 
-
-    // const catDiv = document.createElement("div")
-
-    // catDiv.innerHTML = "<p>McCormick Investigations is looking for the owner of this cat. Be advised: She bites.</p>"
-    // img.appendChild(catDiv)
+    document.getElementById("main").appendChild(img);
+  
 }
+
+
 myCat()
+fetchApi()
+
 // const contactForm = document.createElement("form"); 
 // contactForm.setAttribute("type", "text");
 // document.body.appendChild(contactForm);
@@ -35,6 +35,8 @@ myCat()
 //     contactForm.addEventListener("submit", action)
 // }
 
+// const contactForm = document.createElement("form")
+// contactForm.setAttribute
 
 
 
