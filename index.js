1fbc5f358b15672334f52867da5dcad5e06e2e78
Jessica Nelson <yahwehschica@yahoo.com>
2021-12-15 13:27:53 -0500

    rendered Most Wanted

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const contactButton = document.getElementById("button")
 const submitButton = document.getElementById("submit")
 const baseUrl = "https://api.fbi.gov/@wanted"
+document.querySelector("#api").innerHTML = "<h5>Most Wanted</h5>"
+
 
 const fetchApi = () => {
 fetch(baseUrl)
 .then(res => res.json())
+// .then(data => console.log(data.items.map(renderWanted)))
 .then(data => console.log(data.items))
-}
+    // const renderToDom = data.items.images.map(image => {
+    //     return `<p>Face: ${image.images}</p>`})
+    //     console.log(renderToDom)
+    }
+
+    
+// .catch(error => console.log(error))
+
 
 const myCat = () => {
     const img = new Image ()
@@ -20,7 +30,9 @@ function thankYou() {
     console.log("Thank you for choosing McCormick Investigations. We will contact you within 24 hours.")
 }
 
+// const renderWanted = () => {
 
+// }
 
 function allEventListeners() {
     submitButton.addEventListener("submit", thankYou())
