101433292f4c8e982c4fdbbd6f9ca2685a50fea5
Jessica Nelson <yahwehschica@yahoo.com>
2021-12-29 16:17:03 -0500

    prettied up code

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 
-// const submitButton = document.getElementById("submit")
 const mainDiv = document.getElementById("main")
 const baseUrl = "https://api.fbi.gov/@wanted"
 const previousButton = document.getElementById("previous")
@@ -9,7 +8,6 @@ const form = document.getElementById("form")
 let criminals = []
 let index = 0
  
-
 window.addEventListener("DOMContentLoaded", (e) => {
     form.addEventListener("submit", thankYou)
     fetchApi()
@@ -26,21 +24,18 @@ fetch(baseUrl)
     criminals = data.items
     console.log(criminals)
     renderImage(index)
-     
-})
-}
+    })
+}     
 
- const renderImage = index => {
-       
-    const img = new Image ()
-    img.src = criminals[index].images[0].large
-    img.width ="250"
-    img.height = "300"
-    imgContainer.innerHTML = "<p>Most Wanted</p>" 
-    imgContainer.appendChild(img); 
- }   
-
-
+const renderImage = index => {
+   const img = new Image ()
+   img.src = criminals[index].images[0].large
+   img.width ="250"
+   img.height = "300"
+   imgContainer.innerHTML = "<p>Most Wanted</p>" 
+   imgContainer.appendChild(img); 
+ }      
+    
 const myCat = () => {
     const img = new Image ()
     img.src = "https://img-9gag-fun.9cache.com/photo/aD1n89B_460s.jpg"
@@ -51,25 +46,23 @@ const myCat = () => {
         "<p>Lost Cat. Please contact<br> if you have info.<br> Warning: She bites.</p>"
 }
 
-    function thankYou(e) {
-        e.preventDefault()
-        alert("Thank you for choosing McCormick Investigations. We will contact you within 24 hours.")
-        e.target.reset()
+function thankYou(e) {
+    e.preventDefault()
+    alert("Thank you for choosing McCormick Investigations. We will contact you within 24 hours.")
+    e.target.reset()
+}
+
+const clickNextButton = () => {
+    nextButton.addEventListener("click", () => {
+    renderImage(++index)
+    })
+}  
        
-    }
-
-    const clickNextButton = () => {
-        nextButton.addEventListener("click", () => {
-            renderImage(++index)
-            
-        })
-    }
-
-    const clickPreviousButton = () => {
-        previousButton.addEventListener("click", () => {
-            renderImage(--index)
-        })
-    }
+const clickPreviousButton = () => {
+    previousButton.addEventListener("click", () => {
+    renderImage(--index)
+    })
+}
    
     
 
